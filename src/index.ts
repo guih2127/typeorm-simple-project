a17309d2321e82aa827df5d9511beb0106234b8e
Guilherme Reis Araújo <guih2127@gmail.com>
2022-06-02 16:10:21 -0300

    Working with dataSources, entities and entity Manager

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { AppDataSource } from "./data-source";
+import { Photo } from "./entity/Photo";
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+  .then(async () => {
+    const photo = new Photo();
+    photo.name = "Me and Bears";
+    photo.description = "I am near polar bears";
+    photo.filename = "photo-with-bears.jpg";
+    photo.views = 1;
+    photo.isPublished = true;
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+    await AppDataSource.manager.save(photo);
+    console.log("Photo has been saved. Photo id is", photo.id);
+  })
+  .catch((error) => console.log(error));
