43f5264c1f45a4470dc6d8ddbb9163462e6737bf
Guilherme Reis Araújo <guih2127@gmail.com>
2022-06-03 00:14:36 -0300

    Operations with a repository

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,30 @@ import { Photo } from "./entity/Photo";
 
 AppDataSource.initialize()
   .then(async () => {
-    const photo = new Photo();
-    photo.name = "Me and Bears";
-    photo.description = "I am near polar bears";
-    photo.filename = "photo-with-bears.jpg";
-    photo.views = 1;
-    photo.isPublished = true;
+    const photoRepository = AppDataSource.getRepository(Photo);
+    const allPhotos = await photoRepository.find();
+    console.log("All photos from the db: ", allPhotos);
 
-    await AppDataSource.manager.save(photo);
-    console.log("Photo has been saved. Photo id is", photo.id);
+    const firstPhoto = await photoRepository.findOneBy({
+      id: 1,
+    });
+    console.log("First photo from the db: ", firstPhoto);
+
+    const meAndBearsPhoto = await photoRepository.findOneBy({
+      name: "Me and Bears",
+    });
+    console.log("Me and Bears photo from the db: ", meAndBearsPhoto);
+
+    const allViewedPhotos = await photoRepository.findBy({ views: 1 });
+    console.log("All viewed photos: ", allViewedPhotos);
+
+    const allPublishedPhotos = await photoRepository.findBy({
+      isPublished: true,
+    });
+    console.log("All published photos: ", allPublishedPhotos);
+
+    const [photos, photosCount] = await photoRepository.findAndCount();
+    console.log("All photos: ", photos);
+    console.log("Photos count: ", photosCount);
   })
   .catch((error) => console.log(error));
